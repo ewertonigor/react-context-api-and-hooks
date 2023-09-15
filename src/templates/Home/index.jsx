5e4152cd6d3e3d2a062075cc2a7f6d0455a6057c
Ewerton Igor <ewertonigor@icloud.com>
2023-09-15 11:11:20 -0300

    Criando as ações e o button

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useCounterContext } from '../../contexts/CounterContext';
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
 
 export const App = () => {
   const [state, actions] = useCounterContext();
 
-  useEffect(() => {
-    actions.increase();
-  }, [actions]);
-
   return (
     <div className="App">
-      <h1 onClick={() => actions.increase()}>Lá ele</h1>
+      <Heading />
+
+      <div>
+        <Button onButtonClick={actions.increase}>Increase</Button>
+      </div>
     </div>
   );
 };
